340479ec82897831307443b928e9235b47b79ef2
Rafi Qadri <rqadri133@users.noreply.github.com>
2015-12-22 02:38:30 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+
+'use strict';
+
+
+var app = angular.module('angularServiceDashboard', ['ng.epoch','n3-pie-chart']);
+
+app.value('backendServerUrl', 'http://sitepointsignal.cloudapp.net/');
+app.value('TMServerUrl', 'http://tmservice.cloudapp.net/');
